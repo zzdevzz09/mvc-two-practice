1b16f53cc5c4191a382477638f44e710231d0515
pjy <zzdevzz07@gmail.com>
2023-07-17 23:15:35 +0900

    exception 연습

---
+++ yun-exception/src/main/java/hello/exception/YunExceptionApplication.java
@@ -0,0 +1,13 @@
+package hello.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunExceptionApplication.class, args);
+	}
+
+}
